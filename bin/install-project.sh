5b3c76e8d9213be6dd9ad6790b3e6fcda9426ae0
Damber Prasad Gautam <damber@apnic.net>
2018-12-19 15:14:19 +1000

    Updated project install script

--- bin/install-project.sh
+++ bin/install-project.sh
@@ -2,11 +2,17 @@
 
 rm -rf docker/laravel
 
+# OPTION 1
+# -------------------- #
 # Download laravel LTS framework (https://github.com/laravel/laravel/releases)
 LARAVEL_VERSION_NO="5.5.28"
 curl -L https://github.com/laravel/laravel/archive/v${LARAVEL_VERSION_NO}.tar.gz | tar xz
 mv laravel-${LARAVEL_VERSION_NO} docker/laravel
+cp laravel/.env.example laravel/.env
+# -------------------- #
 
+# OPTION 2
+# -------------------- #
 # OR clone your existing project
 #git clone http://gitlab.apnic.net/laravel/laravel-laravel.git laravel
 #mv laravel docker/laravel
@@ -15,6 +21,7 @@ mv laravel-${LARAVEL_VERSION_NO} docker/laravel
 #cd laravel
 #git checkout develop
 #cd ..
+# -------------------- #
 
 # Create image
 docker-compose up --build -d
@@ -22,13 +29,13 @@ docker-compose up --build -d
 # Perform series of action in container
 APP_NAME="laravel_php_fpm"
 
+docker-compose exec $APP_NAME composer install
+
 docker-compose exec $APP_NAME php artisan key:generate
 
 docker-compose exec $APP_NAME php artisan optimize
 
 docker-compose exec $APP_NAME php artisan migrate
-
-docker-compose exec $APP_NAME composer install
 
 docker-compose exec $APP_NAME node -v
 
